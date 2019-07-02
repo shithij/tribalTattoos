51abd6b3246bbd67ddf7314f9fb80215f253cfc8
sumukh-vasisht <sumukhmys1999@gmail.com>
2019-07-02 20:42:09 +0530

    Changes

--- tattoo/app.py
+++ tattoo/app.py
@@ -12,5 +12,41 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def home() : 
 	return render_template("index2.html")
 
+@app.route("/about")
+def about() : 
+	return render_template("about.html")
+
+@app.route("/services")
+def services() : 
+	return render_template("service3.html")
+
+@app.route("/project")
+def project() : 
+	return render_template("project.html")
+
+@app.route("/booking")
+def booking() : 
+	return render_template("booking.html")
+
+@app.route("/blog")
+def blog() : 
+	return render_template("blog.html")
+
+@app.route("/shop")
+def shop() : 
+	return render_template("shop.html")
+
+@app.route("/team")
+def team() : 
+	return render_template("team.html")
+
+@app.route("/portfolio")
+def portfolio() : 
+	return render_template("team2.html")
+
+@app.route("/contact")
+def contact() : 
+	return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
